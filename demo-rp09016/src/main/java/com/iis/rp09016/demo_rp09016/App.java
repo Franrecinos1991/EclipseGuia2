7ef18d4d3706867fcce930146a5967fd9c28755b
Franrecinos1991 <rp09016@ues.edu.sv>
2025-05-09 00:06:09 -0600

    new commit

---
+++ demo-rp09016/src/main/java/com/iis/rp09016/demo_rp09016/App.java
@@ -0,0 +1,15 @@
+package com.iis.rp09016.demo_rp09016;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public String Hello() {
+        return "Hello World";
+    }
+    
+    public static void main(String[] args) {
+        App hw = new App();
+        System.out.println(hw.Hello());
+    }
+}
